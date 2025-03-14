cea704a01e765be4530cbf41a2fd0a363afb9510
Amir Husain <amir.developer786@outlook.com>
2025-03-14 16:45:33 +0530

    Navigation and Utils added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Navigation from '@navigation/Navigation'
 
 const App = () => {
   return (
-    <View>
-      <Text>Kart App</Text>
-    </View>
+    <Navigation />
   )
 }
 
--- src/modules/onboard/index.tsx
+++ src/modules/onboard/index.tsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React, { FC, useEffect } from 'react'
+import { Colors, screenHeight, screenWidth } from '@utils/Constants'
+import { resetAndNavigate } from '@navigation/NavigationUtil'
 
-const SplashScreen = () => {
+const Splash: FC = () => {
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      resetAndNavigate("Home");
+    }, 2000)
+
+    return () => clearTimeout(timeoutId);
+  }, [])
+
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <Image source={require('@assets/images/logo_t.png')}
+        style={styles.image}
+      />
     </View>
   )
 }
 
-export default SplashScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary
+  },
+  image: {
+    width: screenWidth * 0.35,
+    height: screenHeight * 0.35,
+    resizeMode: 'contain'
+  }
+})
+
+export default Splash
