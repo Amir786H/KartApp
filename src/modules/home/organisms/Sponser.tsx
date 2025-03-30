c5ca5d185c79264c5b33b8ae7a7b8981ef7f1d8d
Amir Husain <amir.developer786@outlook.com>
2025-03-30 18:23:17 +0530

    Categories UI added

---
+++ src/modules/home/organisms/Sponser.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Sponser = () => {
+  return (
+    <View>
+      <Text>Sponser</Text>
+    </View>
+  )
+}
+
+export default Sponser
--- src/modules/home/templates/MainList.tsx
+++ src/modules/home/templates/MainList.tsx
@@ -1,14 +1,92 @@
-import { View, Text } from 'react-native'
-import React, { FC } from 'react'
+import { View, Text, FlatList, NativeSyntheticEvent, NativeScrollEvent, RefreshControl, Platform, ActivityIndicator } from 'react-native'
+import React, { FC, useRef, useState } from 'react'
 import { dynamicDashboardData as fullData } from '@utils/db'
+import AdCarousel from '../organisms/AdCarousel';
+import Categories from '../organisms/Categories';
+import Sponser from '../organisms/Sponser';
 
+const sectionComponents: { [key: string]: React.ComponentType<any> } = {
+  ad_carousal: AdCarousel,
+  categories: Categories,
+  sponser: Sponser
+}
 const PAGE_SIZE = 4;
 
-const MainList:FC<{scrollYGlobal: any}> = ({scrollYGlobal}) => {
+const MainList: FC<{ scrollYGlobal: any }> = ({ scrollYGlobal }) => {
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [data, setData] = useState(fullData.slice(0, PAGE_SIZE));
+  const [currentpage, setCurrentPage] = useState(1);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
+  const prevScrollY = useRef(0);
+  const flatlistRef = useRef(null);
+
+
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const currentScrollY = event?.nativeEvent?.contentOffset.y;
+    scrollYGlobal.value = currentScrollY;
+    prevScrollY.current = currentScrollY;
+  }
+
+  const handleRefresh = () => {
+    setIsRefreshing(true);
+    setTimeout(() => {
+      setCurrentPage(1);
+      setData(fullData?.slice(0, PAGE_SIZE));
+    }, 3000);
+    setIsRefreshing(false);
+  }
+
+  const handleLoadMore = () => {
+    if (isLoadingMore) return;
+    if (data?.length >= fullData?.length) return;
+
+    setIsLoadingMore(true);
+    setTimeout(() => {
+      const nextPage = currentpage + 1;
+      const newItems = fullData?.slice(0, nextPage * PAGE_SIZE);
+      setData(newItems);
+      setCurrentPage(nextPage);
+      setIsLoadingMore(false);
+    }, 4000);
+  }
+
+
+  const renderItem = ({ item }: any) => {
+    const SectionComponent = sectionComponents[item?.type]
+    return SectionComponent ? <SectionComponent data={item} /> : null;
+  }
+
+
+
   return (
-    <View>
-      <Text>MainList</Text>
-    </View>
+    <FlatList
+      data={data}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
+      }
+      overScrollMode='always'
+      onScroll={handleScroll}
+      ref={flatlistRef}
+      scrollEventThrottle={16}
+      renderItem={renderItem}
+      onEndReached={handleLoadMore}
+      onEndReachedThreshold={0.5}
+      nestedScrollEnabled={true}
+      contentContainerStyle={{
+        paddingBottom: Platform.OS === 'android' ? 200 : 300,
+      }}
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(item, index) => index.toString()}
+      ListFooterComponent={isLoadingMore ? (
+        <ActivityIndicator
+          size={'small'}
+          color="#888"
+          style={{ alignSelf: 'center', margin: 15 }}
+        />
+      ) : null}
+    />
   )
 }
 
